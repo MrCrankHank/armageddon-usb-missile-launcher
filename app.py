a08b485c0930f07ebc2bed9103f5e97bbc803143
Alexander Hank <alexander.hank@lra-aic-fdb.de>
2015-07-27 13:44:04 +0200

    reduce sleep time

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ class ArmageddonByArrows(QtGui.QMainWindow):
     def fire_all(self):
         for x in range (0, 4):
             self.armageddon.send_cmd(self.armageddon.FIRE)
-            sleep(3.3)
+            sleep(3.2)
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
